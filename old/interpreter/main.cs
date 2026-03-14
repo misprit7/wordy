f9a4927b2dbd8e1d8a6a2514c38b1373e0b1eb34
misprit7 <xandernaumenko@gmail.com>
2026-03-14 12:57:23 -0400

    Moved old code to old

---
+++ old/interpreter/main.cs
@@ -0,0 +1,13 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+Compiler compiler = new Compiler("../samples/docx/wordy-test-2.docx");
+
+compiler.parseFunction();
+/* compiler.PrintDoc(); */
+/* compiler.BodyElements(); */
+
+
+
+
+
